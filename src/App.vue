dad47d7a8d5bc784d5dfe3840914645fef7c81e5
Charlilyyyy <mohamadtaufik099@gmail.com>
2023-11-20 22:45:03 +0800

    Card and data

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,22 @@
       <input v-model.trim="search" type="text" placeholder="Search..."/>
     </header>
     <div class="cards-container">
-      <div v-for="quiz in quizzes" :key="quiz.id" class="card">
+      <Card v-for="quiz in quizzes" :key="quiz.id" :quiz="quiz"/>
+      <!-- <div v-for="quiz in quizzes" :key="quiz.id" class="card">
         <img :src="quiz.img" alt="">
         <div class="card-text">
           <h2>{{ quiz.name }}</h2>
           <p>{{ quiz.questions.length }} Question</p>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
 
 <script setup>
 import Quizzes from "./data/quizzes.json";
+import Card from "./components/Card.vue";
+
 import { ref,watch } from "vue";
 
 const quizzes = ref(Quizzes)
@@ -61,28 +64,5 @@ watch(search, () => {
 
   /*CARD*/
 
-  .card{
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-    margin: 0 20px 35px 0;
-    cursor: pointer;
-    background-color: white;
-  }
   
-  .card img{
-    width: 100%;
-    height: 190px;
-    margin: 0;
-  }
-
-  .card .card-text{
-    padding: 0 0 10px 10px;
-    color: black;
-  }
-
-  .card .card-text h2{
-    font-weight: bold;
-  }
 </style>
